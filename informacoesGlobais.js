e48a7094917f8adc389b64de6198017bf41d49f7
Bruno <126162420+bruno-bertolini@users.noreply.github.com>
2024-10-04 08:57:03 -0300

    m

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -8,6 +8,6 @@ async function visualizarInfomacoesGlobais() {
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    console.log(paragrafo);    
+       
 }
 visualizarInfomacoesGlobais()
